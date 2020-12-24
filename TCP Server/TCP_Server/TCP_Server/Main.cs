6128f2e18f2b5794bc1803a5b64461e721c2a43f
doesluck1026 <58068335+doesluck1026@users.noreply.github.com>
2020-12-24 23:23:04 +0300

    finished TCPServer.cs
    Server side codes of communication is completed. Main.cs class is added

---
+++ TCP Server/TCP_Server/TCP_Server/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Main
+{
+    public string ServerIP;
+    public string ClientIP;
+
+    private TCPServer Server;
+    public readonly int Port = 38000;
+    private readonly byte StartByte = (byte)'J';
+    public void StartServer()
+    {
+        Server = new TCPServer(Port, startByte: StartByte);
+        ServerIP = Server.SetupServer();
+        ClientIP = Server.StartListener();
+    }
+}
