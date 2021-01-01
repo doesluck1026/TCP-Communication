334ba52d41ce0168177fa28a7bdc7e8cc91498db
doesluck1026 <58068335+doesluck1026@users.noreply.github.com>
2021-01-01 21:20:20 +0300

    finished

---
+++ TCP Server/TCP_Server/TCP_Server/Core Classes/Main.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+class Main
+{
+    #region Public Variables
+    public string ServerIP;
+    public string ClientIP;
+
+    public Color LedColor
+    {
+        get
+        {
+            lock (Lck_LedColor)
+                return _ledColor;
+        }
+        private set
+        {
+            lock (Lck_LedColor)
+                _ledColor = value;
+        }
+    }
+    public string ClientMessage
+    {
+        get
+        {
+            lock (Lck_ClientMessage)
+                return _clientMessage;
+        }
+        private set
+        {
+            lock (Lck_ClientMessage)
+                _clientMessage = value;
+        }
+    }
+    public int TargetSize
+    {
+        get
+        {
+            lock (Lck_TargetSize)
+                return _targetSize;
+        }
+        private set
+        {
+            lock (Lck_TargetSize)
+                _targetSize = value;
+        }
+    }
+
+    private Color _ledColor;
+    private string _clientMessage;
+    private int _targetSize;
+
+    private object Lck_LedColor = new object();
+    private object Lck_ClientMessage = new object();
+    private object Lck_TargetSize = new object();
+    #endregion
+
+    #region Parameters 
+
+    public readonly int Port = 38001;
+    private readonly byte StartByte = (byte)'J';
+    private int CommmunicationFrequency = 50;       /// Hz
+
+    #endregion
+
+    #region Private Variables
+
+    private TCPServer Server;
+    private Thread CommunicationThread;
+    private double CommunicationPeriod;
+    private bool ThreadEnabled = false;
+    #endregion
+
+    #region Indexes
+
+    #region Client To Server
+        private readonly int Index_LedColor = 0;
+        private readonly int Index_ClientMessage = 3;
+    #endregion
+    #region Server To Client
+    private readonly int Index_TargetSize = 0;
+    #endregion
+
+    #endregion
+    public Main()
+    {
+
+    }
+    private void StartServer()
+    {
+        Server = new TCPServer(port: Port, StartByte: StartByte);
+        ServerIP = Server.SetupServer();
+        ClientIP = Server.StartListener();
+    }
+    public void StartCommunicationThread()
+    {
+        CommunicationPeriod = 1.0 / CommmunicationFrequency;
+        CommunicationThread = new Thread(CoreFcn);
+        ThreadEnabled = true;
+        CommunicationThread.Start();
+    }
+    public void StopCommunication()
+    {
+        Server.CloseServer();
+        Server = null;
+        ServerIP = "";
+        ClientIP = "";
+        if (CommunicationThread!=null)
+        {
+            ThreadEnabled = false;
+            if (CommunicationThread.IsAlive)
+                CommunicationThread.Abort();
+            CommunicationThread = null;
+        }
+        
+    }
+    private void CoreFcn()
+    {
+        Stopwatch watch = Stopwatch.StartNew();
+        StartServer();
+        while (ThreadEnabled)
+        {
+            SendClientData();
+            GetClientData();
+
+            while (watch.Elapsed.TotalSeconds<CommunicationPeriod)
+            {
+                Thread.Sleep(1);
+            }
+            watch.Restart();
+        }
+    }
+    private byte[] PrepareDataToBeSent()
+    {
+        byte[] targetSizeBytes = BitConverter.GetBytes(TargetSize);
+        return targetSizeBytes;
+    }
+    private void SendClientData()
+    {
+       byte[] data=  PrepareDataToBeSent();
+        Server.SendDataToClient(data);
+    }
+    private void GetClientData()
+    {
+        byte[] data = Server.GetData();
+        AnalyzeReceivedData(data);
+    }
+    private void AnalyzeReceivedData(byte[] receivedData)
+    {
+        byte[] ColorBytes = new byte[3];
+        byte[] MessageBytes;
+        Array.Copy(receivedData, Index_LedColor, ColorBytes, 0, ColorBytes.Length);
+        AssignLedColor(ColorBytes);
+        int LenMessage = receivedData[Index_ClientMessage] | (receivedData[Index_ClientMessage + 1] << 8);
+        MessageBytes = new byte[LenMessage];
+        Array.Copy(receivedData, Index_ClientMessage + 2, MessageBytes, 0, LenMessage);
+        AssignMessage(MessageBytes);
+    }
+    private void AssignLedColor(byte[] colorBytes)
+    {
+        LedColor = Color.FromRgb(colorBytes[0], colorBytes[1], colorBytes[2]);
+    }
+    private void AssignMessage(byte[] messageBytes)
+    {
+        ClientMessage = Encoding.ASCII.GetString(messageBytes);
+    }
+}
---
+++ TCP Server/TCP_Server/TCP_Server/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TCP_Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Main main;
+        private Timer UIUpdateTimer;
+        private int UiUpdateFrequency = 30; /// Hz
+        private int UiUpdatePeriod;             // milliseconds
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_StartServer_Click(object sender, RoutedEventArgs e)
+        {
+            main = new Main();
+            main.StartCommunicationThread();
+            StartUpdateTimer();
+        }
+        private void UpdateUI()
+        {
+            if (main == null)
+                return;
+            Dispatcher.Invoke(() =>
+            {
+                if (main.LedColor != null)
+                    lbl_Led.Background = new SolidColorBrush(main.LedColor);
+                if (!string.IsNullOrEmpty(main.ClientMessage))
+                    txt_ClientMessage.Text = main.ClientMessage;
+                lbl_ClientIP.Content = main.ClientIP;
+                txt_ServerIP.Text = main.ServerIP;
+            });
+        }
+        private void StartUpdateTimer()
+        {
+            UiUpdatePeriod = (int) (1000.0 / UiUpdateFrequency);
+            UIUpdateTimer = new Timer(Timer_Tick, null, 0, UiUpdatePeriod);
+        }
+        private void StopUpdateTimer()
+        {
+            if(UIUpdateTimer!=null)
+            {
+                UIUpdateTimer.Change(Timeout.Infinite, Timeout.Infinite);
+                UIUpdateTimer.Dispose();
+                UIUpdateTimer = null;
+            }
+        }
+        private void Timer_Tick(object state)
+        {
+            if (UIUpdateTimer == null)
+                return;
+            UIUpdateTimer.Change(Timeout.Infinite, Timeout.Infinite);       /// to avoid multiple events
+            var watch = Stopwatch.StartNew();
+            UpdateUI();
+            if (UIUpdateTimer == null)
+                return;
+            UIUpdateTimer.Change((int)Math.Max(0, UiUpdatePeriod - watch.ElapsedMilliseconds), UiUpdatePeriod);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            StopUpdateTimer();
+            if(main!=null)
+             main.StopCommunication();
+        }
+    }
+}
